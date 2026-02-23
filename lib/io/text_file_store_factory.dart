8b027885af3373d9a0fc7a2b975263cdb910278f
Alex Wan <wana@usc.edu>
2026-02-22 19:53:42 -0800

    removed unnecessary export line

--- lib/io/text_file_store_factory.dart
+++ lib/io/text_file_store_factory.dart
@@ -3,7 +3,4 @@ import 'package:omnilore_scheduler/io/text_file_store_stub.dart'
     if (dart.library.io) 'package:omnilore_scheduler/io/text_file_store_io.dart'
     as impl;
 
-export 'package:omnilore_scheduler/io/web_download_stub.dart'
-    if (dart.library.html) 'package:omnilore_scheduler/io/web_download_web.dart';
-
 TextFileStore createTextFileStore() => impl.createTextFileStoreImpl();
