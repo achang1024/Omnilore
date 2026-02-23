9d3547628c13227f4ad762ae799e5f6b84df56e1
Alex Wan <wana@usc.edu>
2026-02-22 20:00:42 -0800

    Revert "Intermediate state persistence merge attempt"

--- lib/io/web_download.dart
+++
@@ -1,4 +0,0 @@
-/// Triggers a browser file download. No-op on desktop.
-void triggerDownload(String content, String filename) {
-  // Desktop doesn't use this path
-}
--- lib/io/web_download_stub.dart
+++
@@ -1,3 +0,0 @@
-void triggerDownload(String content, String filename) {
-  // Desktop doesn't use this path
-}
